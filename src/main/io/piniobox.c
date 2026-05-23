3062b51fc5d2885e71afd683fb88b52cefda1fe0
Oleg Moshenskiy <o.moshenskyi@ukma.edu.ua>
2026-05-23 20:37:11 +0300

    Add custom relay logic to piniobox.c

--- src/main/io/piniobox.c
+++ src/main/io/piniobox.c
@@ -38,6 +38,9 @@
 
 #include "piniobox.h"
 
+#include "rx/rx.h"
+#include "fc/core.h"
+
 typedef struct pinioBoxRuntimeConfig_s {
     uint8_t boxId[PINIO_COUNT];
 } pinioBoxRuntimeConfig_t;
@@ -55,6 +58,47 @@ void pinioBoxInit(const pinioBoxConfig_t *pinioBoxConfig)
     }
 }
 
+static uint32_t customRelayActivationTime = 0;
+static bool customRelayHasFired = false;
+static bool customRelayIsActive = false;
+
+static void updateCustomRelay(void) {
+    // Отримуємо статус арму та значення газу
+    bool isArmed = ARMING_FLAG(ARMED);
+    uint16_t throttlePwm = rcData[THROTTLE];
+
+    // скидання всіх станів
+    if (!isArmed) {
+        customRelayHasFired = false;
+        if (customRelayIsActive) {
+            pinioSet(0, false); // Примусово вимикаємо PINIO 1 (M7)
+            customRelayIsActive = false;
+        }
+        return;
+    }
+
+    // Заармлено + Повний газ + Ще не стріляло
+    if (isArmed && throttlePwm > 1950 && !customRelayHasFired) {
+        customRelayActivationTime = millis();
+        customRelayHasFired = true;
+        customRelayIsActive = true;
+        pinioSet(0, true); // Вмикаємо PINIO 1 на 3.3V
+    }
+
+    // Логіка таймера на 4 секунди
+    if (customRelayIsActive) {
+        if ((millis() - customRelayActivationTime) >= 4000) {
+            pinioSet(0, false); // Знімаємо напругу через 4 секунди
+            customRelayIsActive = false;
+        } else {
+            pinioSet(0, true);  // Підтримуємо 3.3V, поки час не вийшов
+        }
+    }
+}
+
+
+
+
 void pinioBoxUpdate(timeUs_t currentTimeUs)
 {
     UNUSED(currentTimeUs);
@@ -64,17 +108,12 @@ void pinioBoxUpdate(timeUs_t currentTimeUs)
             pinioSet(i, getBoxIdState(pinioBoxRuntimeConfig.boxId[i]));
         }
     }
+
+    updateCustomRelay();
 }
 
 void pinioBoxTaskControl(void)
 {
-    bool enableTask = false;
-    for (int i = 0; i < PINIO_COUNT; i++) {
-        if (pinioBoxRuntimeConfig.boxId[i] != BOXID_NONE && isModeActivationConditionPresent(pinioBoxRuntimeConfig.boxId[i])) {
-            enableTask = true;
-            break;
-        }
-    }
-    setTaskEnabled(TASK_PINIOBOX, enableTask);
+ setTaskEnabled(TASK_PINIOBOX, true);
 }
 #endif
